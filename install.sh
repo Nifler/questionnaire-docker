207bb5dfd3e8f66736832b97da8c79afc224d41b
Nifler <nifler11@gmail.com>
2023-05-11 13:47:51 +0300

    Working install.sh without private repository and enviroment

---
+++ install.sh
@@ -0,0 +1,5 @@
+git clone git@github.com:Nifler/laravel-box.git www
+GD=$(id -g ${USER}) UD=$(id -u ${USER}) docker-compose up -d --build
+docker-compose exec php composer install
+docker-compose exec php cp .env.example .env
+docker-compose exec php php artisan key:generate
